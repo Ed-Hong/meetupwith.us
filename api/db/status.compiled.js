81e1059138fa33e216b1d9eb8a835f46ce278273
Edward Hong <edward.hong@yahoo.com>
2016-12-23 21:58:14 -0600

    * Add config files * Attempted to run unit tests - still not working atm

--- api/db/status.compiled.js
+++ api/db/status.compiled.js
@@ -7,8 +7,6 @@ exports.findOneAndUpdate = exports.findOne = exports.create = undefined;
 
 var _bluebird = require('bluebird');
 
-// const Status = models.Status;
-
 /**
  * Creates a Status object based off the schema
  * @param {*} attributes: the attributes to add to the hour of the producers
@@ -17,7 +15,7 @@ var _bluebird = require('bluebird');
 
 let create = exports.create = (() => {
   var _ref = (0, _bluebird.coroutine)(function* (attributes) {
-    return yield new _status2.default(attributes).save();
+    return yield new Status(attributes).save();
   });
 
   return function create(_x) {
@@ -33,7 +31,7 @@ let create = exports.create = (() => {
 
 let findOne = exports.findOne = (() => {
   var _ref2 = (0, _bluebird.coroutine)(function* (attributes) {
-    return yield _status2.default.findOne(attributes).exec();
+    return yield Status.findOne(attributes).exec();
   });
 
   return function findOne(_x2) {
@@ -45,7 +43,7 @@ let findOneAndUpdate = exports.findOneAndUpdate = (() => {
   var _ref3 = (0, _bluebird.coroutine)(function* (conditions, updates) {
     let options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
 
-    const status = yield _status2.default.findOneAndUpdate(conditions, updates, options).exec();
+    const status = yield Status.findOneAndUpdate(conditions, updates, options).exec();
     if (Utils.isEmpty(status)) {
       throw new Error(`Could not find and update status with attributes: ${ conditions } with updates ${ updates }`);
     }
@@ -57,10 +55,12 @@ let findOneAndUpdate = exports.findOneAndUpdate = (() => {
   };
 })();
 
-var _status = require('../../models/mongo/status.compiled.js');
+var _index = require('../../models/mongo/index.compiled.js');
 
-var _status2 = _interopRequireDefault(_status);
+var _index2 = _interopRequireDefault(_index);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Status = _index2.default.Status;
 
 //# sourceMappingURL=status.compiled.js.map
